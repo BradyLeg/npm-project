3b0973bad8e79a31efd7f58be98624772566f6f1
Squizard <184549450+BradyLeg@users.noreply.github.com>
2025-04-04 18:32:22 -0700

    used faker, colors and validator packages

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 import lodash from 'lodash';
 import chalk from 'chalk';
+import { faker } from '@faker-js/faker';
+import colors from 'colors';
+import validator from 'validator';
+
+let fakeAccount = {
+    userId: faker.string.uuid(),
+    userName: faker.internet.userName(),
+    email: faker.internet.email(),
+    registerAt: faker.date.past()
+};
+
+console.log(fakeAccount.userId);
+console.log(fakeAccount.userName)
+console.log(fakeAccount.email)
+console.log(fakeAccount.registerAt)
+
+const validEmail = 'shiro@gmail.com';
+const invalidEmail = 'serika@231';
+
+console.log(validEmail.cyan, validator.isEmail(validEmail));
+console.log(invalidEmail.red, validator.isEmail(invalidEmail));
 
 const array = [1, 2, 3, 4, 1, 6, 9, 2, 9, 3];
 const uniqueNums = lodash.uniq(array);
